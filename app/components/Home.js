408fd585af525ce7e07eb7692db7e01fedcd9507
Alex DeCapri <alexdecapri@Alexs-MacBook-Pro.local>
2016-08-03 11:15:42 -0600

    Update basic bootstrap style for home page

--- app/components/Home.js
+++ app/components/Home.js
@@ -1,9 +1,28 @@
 var React = require('react');
 
+var styles = {
+  welcome: {
+    fontSize: '50px',
+    marginBottom: '40px'
+  },
+  header: {
+    fontSize: '30px',
+    marginBottom: '40px'
+  },
+  button: {
+    fontSize: '25px'
+  }
+};
+
+
 var Home = React.createClass({
   render: function() {
     return (
-      <div>Home</div>
+      <div className="text-center">
+        <div style={styles.welcome}>Welcome to Poli-Dash</div>
+        <div style={styles.header}>News & Info for Things You Actually Care About.</div>
+        <button className="btn btn-success" style={styles.button}>Get Started!</button>
+      </div>
     )
   }
 })
